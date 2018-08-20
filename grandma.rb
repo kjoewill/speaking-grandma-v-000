3c429063a67de39ea467678a4a3091322bc0c1a2
Kevin W <kjoewill@gmail.com>
2018-08-20 18:55:13 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -12,9 +12,8 @@ def speak_to_grandma(phrase)
   
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  end
   
-  if phrase == all_caps
+  elseif phrase == all_caps
     return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
